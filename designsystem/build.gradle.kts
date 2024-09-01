a3f215d53fc9153341902fc8b10a680f6387214c
MarjanaKarzek <marjana.karzek@outlook.com>
2024-09-01 15:07:23 +0200

    use java 17

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
   alias(libs.plugins.android.library)
   alias(libs.plugins.jetbrains.kotlin.android)
-  id("app.cash.paparazzi") version "1.3.4"
+  alias(libs.plugins.paparazzi.plugin)
 }
 
 android {
@@ -22,11 +22,11 @@ android {
     }
   }
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
   kotlinOptions {
-    jvmTarget = "1.8"
+    jvmTarget = "17"
   }
   buildFeatures {
     compose = true
---
+++ restaurants/src/test/java/com/karzek/restaurants/data/RestaurantRepositoryImplTest.kt
@@ -0,0 +1,5 @@
+package com.karzek.restaurants.data
+
+class RestaurantRepositoryImplTest {
+
+}
